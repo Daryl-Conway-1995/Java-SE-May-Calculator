f3bc5a9ee50f5e2c9a392953341b4341090f76eb
Daryl-Conway-1995 <darylconway1995@gmail.com>
2018-07-11 11:45:46 +0100

    changed naming conventions

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -4,22 +4,22 @@ import functions.*;
 public class Calculator {
 
 	public int add(int i, int j) {
-		Adder add = new Adder();
-		return add.add(i,j);
+		Adder adder = new Adder();
+		return adder.add(i,j);
 	}
 
 	public int subtract(int i, int j) {
-		Subtractor subtract = new Subtractor();
-		return subtract.subtract(i,j);
+		Subtractor subtractor = new Subtractor();
+		return subtractor.subtract(i,j);
 	}
 
 	public double divide(int i, int j) {
-		Divider divide = new Divider();
-		return divide.divide(i,j);
+		Divider divider = new Divider();
+		return divider.divide(i,j);
 	}
 
 	public int mult(int i, int j) {
-		Multiplier multiply = new Multiplier();
-		return multiply.multiply(i,j);
+		Multiplier multiplier = new Multiplier();
+		return multiplier.multiply(i,j);
 	}
 }
